b6454b74977f6fddb38ef4bd7f016afe2d409b18
igor <perrofeofilms@gmail.com>
2019-09-07 14:41:09 -0600

    sw

--- js/main.js
+++ js/main.js
@@ -694,3 +694,19 @@ document.querySelectorAll('.js-figure-link').forEach((l) => {
     }
   });
 });
+
+//service worker
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    console.log("An active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("{{ site.baseurl }}/serviceworker.js", {
+        scope: "./"
+      })
+      .then(function (reg) {
+        console.log("Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+}
